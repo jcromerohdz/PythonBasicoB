8cacca5ceb064f5eaf2221bd45003bd6d93b291e
jcromerohdz <jcromerohdz@gmail.com>
2026-02-10 19:37:49 -0800

    Agregue como usar condicionales en python

--- sesion_01.py
+++ sesion_01.py
@@ -12,4 +12,22 @@ print("===== Operadores Matematicos========")
 # /
 # **
 # % Modulo
+print(int(2**3))
+print(int(4**8))
+print(float(10%2))
+print(float(25%4))
 
+ventas = 1999999
+print("Nuestras ventas fueron: ", ventas)
+
+is_active = True
+print(bool(is_active))
+
+game_over = False
+print(game_over)
+
+edad = 16
+if (edad >= 18):
+    print("Si puedo entrar a el Bar!")
+else:
+    print("No puedo entrar a el Bar!")
